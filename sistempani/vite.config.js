5edf57cd0645caccc065be84d0fee7db2395f00e
Rodrigo <rodrigoss1053@gmail.com>
2026-05-06 17:15:45 -0300

    limpo e prontolll

--- sistempani/vite.config.js
+++ sistempani/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Verifique se essa linha existe
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // E essa aqui também
+  ],
 })
